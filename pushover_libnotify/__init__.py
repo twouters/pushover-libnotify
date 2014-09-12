e59c0c7f278b0c1e9c7e27e9a11e2f6b603b6fb5
Thomas Wouters <thomas@kangaroot.net>
2014-09-12 13:52:35 +0200

    Invalid module name, use underscore

--- pushover_libnotify/__init__.py
+++ pushover_libnotify/__init__.py
@@ -13,56 +13,65 @@ import websocket
 import threading
 import hashlib
 
+def main():
+    config_file = os.path.join(xdg_config_home,'pushover-libnotify.conf')
+    config = configparser.RawConfigParser(allow_no_value=True)
+    if not os.path.isfile(config_file):
+        config['pushover'] = {
+                'userkey': '',
+                'usersecret': '',
+                'deviceid': '',
+                'icon_size': [32,32]
+                }
+        with open(config_file, 'w') as cf:
+            os.chmod(config_file, 0o600)
+            config.write(cf)
 
-config_file = os.path.join(xdg_config_home,'pushover-libnotify.conf')
-config = configparser.RawConfigParser(allow_no_value=True)
-if not os.path.isfile(config_file):
-    config['pushover'] = {
-            'userkey': '',
-            'usersecret': '',
-            'deviceid': '',
-            'icon_size': [32,32]
-            }
-    with open(config_file, 'w') as cf:
-        os.chmod(config_file, 0o600)
-        config.write(cf)
-
-config.read(config_file)
-c = config['pushover']
-
-if (c['userkey'] == '' or c['usersecret'] == ''):
-    user = input('Pushover email: ')
-    password = getpass.getpass('Pushover password (will not be saved): ')
-    r = requests.post(
-            'https://api.pushover.net/1/users/login.json',
-            params=({'email':user, 'password':password})
-            )
-    result = r.json()
-    if not result['status'] == 1:
-        print('Unable to login. %s' % r.text)
-        sys.exit()
-    config['pushover']['userkey'] = result['id']
-    config['pushover']['usersecret'] = result['secret']
-    with open(config_file, 'w') as cf:
-        config.write(cf)
     config.read(config_file)
     c = config['pushover']
 
-if (c['deviceid'] == ''):
-    devicename = os.uname()[1] + '-pushover-libnotify'
-    r = requests.post(
-            'https://api.pushover.net/1/devices.json',
-            params=({'secret': c['usersecret'], 'name': devicename, 'os': 'O'})
-            )
-    result = r.json()
-    if not result['status'] == 1:
-        print('Could not register device: %s' % result['errors'])
-        sys.exit()
-    config['pushover']['deviceid'] = result['id']
-    with open(config_file, 'w') as cf:
-        config.write(cf)
-    config.read(config_file)
-    c = config['pushover']
+    if (c['userkey'] == '' or c['usersecret'] == ''):
+        user = input('Pushover email: ')
+        password = getpass.getpass('Pushover password (will not be saved): ')
+        r = requests.post(
+                'https://api.pushover.net/1/users/login.json',
+                params=({'email':user, 'password':password})
+                )
+        result = r.json()
+        if not result['status'] == 1:
+            print('Unable to login. %s' % r.text)
+            sys.exit()
+        config['pushover']['userkey'] = result['id']
+        config['pushover']['usersecret'] = result['secret']
+        with open(config_file, 'w') as cf:
+            config.write(cf)
+        config.read(config_file)
+        c = config['pushover']
+
+    if (c['deviceid'] == ''):
+        devicename = os.uname()[1] + '-pushover-libnotify'
+        r = requests.post(
+                'https://api.pushover.net/1/devices.json',
+                params=({'secret': c['usersecret'], 'name': devicename, 'os': 'O'})
+                )
+        result = r.json()
+        if not result['status'] == 1:
+            print('Could not register device: %s' % result['errors'])
+            sys.exit()
+        config['pushover']['deviceid'] = result['id']
+        with open(config_file, 'w') as cf:
+            config.write(cf)
+        config.read(config_file)
+        c = config['pushover']
+
+    ws = websocket.WebSocketApp(
+            "wss://client.pushover.net/push",
+            on_message = on_message,
+            on_error = on_error,
+            on_close = on_close)
+
+    ws.on_open = on_open
+    ws.run_forever()
 
 def geticon(icon):
     url = 'https://api.pushover.net/icons/%s.png' % (icon)
@@ -125,11 +134,5 @@ def on_close(ws):
 def on_open(ws):
     ws.send("login:%s:%s\n" % (c['deviceid'], c['usersecret']))
 
-ws = websocket.WebSocketApp(
-        "wss://client.pushover.net/push",
-        on_message = on_message,
-        on_error = on_error,
-        on_close = on_close)
-
-ws.on_open = on_open
-ws.run_forever()
+if __name__ == "__main__":
+    main()
